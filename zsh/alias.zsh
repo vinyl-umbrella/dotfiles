8787c8b36bfba877c3b9838d5c9d2ea8def61a1d
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2025-07-01 23:54:55 +0900

    add base91 encode command

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -34,3 +34,51 @@ function psgrep() {
     ps aux | head -n 1;
     ps aux | grep $* | grep -v "ps -auxww" | grep -v grep;
 }
+
+
+function base91() {
+  local alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#\$%&()*+,./:;<=>?@[]^_\`{|}~\""
+  local input="$1"
+
+  # If no input provided, read from stdin
+  if [[ -z "$input" ]]; then
+    input=$(cat)
+  fi
+
+  # Base91 encode
+  local v=0
+  local b=0
+  local n=0
+  local output=""
+
+  # Convert input to bytes
+  for ((i=0; i<${#input}; i++)); do
+    local char="${input:$i:1}"
+    local byte=$(printf '%d' "'$char")
+
+    b=$((b | (byte << n)))
+    n=$((n + 8))
+
+    while [[ $n -gt 13 ]]; do
+      v=$((b & 8191))
+      if [[ $v -gt 88 ]]; then
+        b=$((b >> 13))
+        n=$((n - 13))
+      else
+        v=$((b & 16383))
+        b=$((b >> 14))
+        n=$((n - 14))
+      fi
+      output+="${alphabet:$((v % 91)):1}${alphabet:$((v / 91)):1}"
+    done
+  done
+
+  if [[ $n -gt 0 ]]; then
+    output+="${alphabet:$((b % 91)):1}"
+    if [[ $n -gt 7 ]] || [[ $b -gt 90 ]]; then
+      output+="${alphabet:$((b / 91)):1}"
+    fi
+  fi
+
+  echo "$output"
+}
